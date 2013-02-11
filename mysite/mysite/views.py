30b95e14277e4be53790a2684274cfc00c9c1ae6
David Peters <dpeters@mediatemple.net>
2013-02-11 15:10:09 -0800

    setting up djcode

---
+++ mysite/mysite/views.py
@@ -0,0 +1,46 @@
+from django.template.loader import get_template
+from django.template import Context
+from django.http import Http404, HttpResponse
+from django.shortcuts import render_to_response
+import datetime
+
+
+
+def hello(request):
+    # say hello
+    return HttpResponse("Hello world")
+
+# current_datetime w/o template
+def current_datetime(request):
+    # get the current time
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    
+    return HttpResponse(html)
+
+# current_datetime w/ template
+def current_datetime_template(request):
+    now = datetime.datetime.now()
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
+
+def more_current_datetime_template(request):
+    now = datetime.datetime.now()
+    return render_to_response('more_current_datetime.html', {'current_date': now})
+
+def hours_ahead(request, hours):
+    # time ahead by said hours
+    try:
+        hours = int(hours)
+    except ValueError:
+        raise Http404()
+
+    dt = datetime.datetime.now() + datetime.timedelta(hours=hours)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (hours, dt)
+
+    # At any point in your view, temporarily insert an assert False to trigger the error page. 
+    # Then you can view the local variables and state of the program.
+    # assert False
+
+    return HttpResponse(html)
